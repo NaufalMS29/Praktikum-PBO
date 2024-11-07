e0f6e97c0e25f3ef17d32b2a456cbb1512e979b1
NaufalMS29 <naufalms2005@gmail.com>
2024-11-07 19:04:02 +0700

    Praktikum Kesembilan

---
+++ PraktikumKesembilan/Project/com/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        ATM atm = new ATM("Jakarta");
+        ATM.BankAccount account = atm.new BankAccount("0011223344");
+        account.displayAccountInfo();
+        account.deposit(1000000);
+        account.displayAccountInfo();
+        account.withdraw(50000);
+        account.displayAccountInfo();
+    }
+}
